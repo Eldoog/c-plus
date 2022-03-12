5859a6e890ab68ae0d52be171f02dfd43bc2df18
andy <a95qu0402@gmail.com>
2022-03-12 11:08:10 +0800

    print&scanf

---
+++ 0312_cin&cout/cin&cout.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	std::cout <<"enter two numbers:"<<std::endl;
+	int v,u;
+	std::cin>>v>>u;
+	std::cout <<"the sum of "<<v<< " and " <<<< " is " << v+u << std ::endl;
+	return 0;
+}
